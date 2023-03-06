b79749d806a3c267fb09e4e757d16936468f90af
Prgckwb <ckwb.uec@gmail.com>
2023-03-06 21:01:11 +0900

    change: some

--- chatgpt/chatgpt.py
+++ chatgpt/chatgpt.py
@@ -12,14 +12,66 @@ class ChatGPTRole:
 
 @dataclasses.dataclass
 class ChatGPTMessage:
-    role: str
     content: str
+    role: str
 
     @classmethod
     def from_json(cls, json_data):
         role = json_data["role"]
         content = json_data["content"]
-        return cls(role, content)
+        return cls(role=role, content=content)
+
+
+@dataclasses.dataclass
+class ChatGPTInput:
+    messages: list[ChatGPTMessage] | ChatGPTMessage
+    temperature: float = 1.0
+    top_p: float = 1.0
+    num_choices_per_input: int = 1
+    stream: bool = False
+    stop: str | list[str] = None
+    max_tokens: int = None
+    presence_penalty: float = 0.0
+    frequency_penalty: float = 0.0
+    logit_bias = None
+    user: str = None
+
+    def __post_init__(self):
+        # TODO: 条件指定をする
+        # assert self.temperature == 1
+        if isinstance(self.messages, ChatGPTMessage):
+            self.messages = [self.messages]
+
+    def to_json_inputs(self):
+        args = dict()
+
+        if isinstance(self.messages, ChatGPTMessage):
+            msg = [self.messages]
+        elif isinstance(self.messages, list):
+            msg = self.messages
+        else:
+            raise TypeError
+
+        args["messages"] = [dataclasses.asdict(m) for m in msg]
+        args["temperature"] = self.temperature
+        args["top_p"] = self.top_p
+        args["n"] = self.num_choices_per_input
+        args["stream"] = self.stream
+
+        if self.stop is not None:
+            args["stop"] = self.stop
+        if self.max_tokens is not None:
+            args["max_tokens"] = self.max_tokens
+
+        args["presence_penalty"] = self.presence_penalty
+        args["frequency_penalty"] = self.frequency_penalty
+
+        if self.logit_bias is not None:
+            args["logit_bias"] = self.logit_bias
+        if self.user is not None:
+            args["user"] = self.user
+
+        return args
 
 
 @dataclasses.dataclass
@@ -81,19 +133,26 @@ class ChatGPT:
     def create_message(content: str, role: str = ChatGPTRole.user) -> ChatGPTMessage:
         """Create a chat history to be used for ChatGPT.
 
-        :param content: Chat content, sentence.
-        :param role: Argument to determine who is speaking, selected from `ChatGPTRole`.
-        :return:
+        Args:
+            content: Chat content, sentence.
+            role: Argument to determine who is speaking, selected from `ChatGPTRole`.
+
+        Returns:
+
         """
 
-        message = ChatGPTMessage(role, content)
+        message = ChatGPTMessage(content=content, role=role)
         return message
 
     def count_token(self, sentence: str) -> int:
         """Calculate the number of tokens used in the ChatGPT
 
-        :param sentence: Sentence for which you want to calculate tokens.
-        :return: Number of tokens.
+        Args:
+            sentence: Sentence for which you want to calculate tokens.
+
+        Returns:
+            int: Number of tokens.
+
         """
 
         encoding = tiktoken.encoding_for_model(self.model)
@@ -101,39 +160,59 @@ class ChatGPT:
         num_tokens = len(tokens)
         return num_tokens
 
-    def request(self, messages: list[ChatGPTMessage], save_history: bool = True) -> ChatGPTOutput:
+    def request(self,
+                inputs: ChatGPTInput,
+                save_history: bool = True) -> ChatGPTOutput:
         """Get data when querying ChatGPT's API.
 
-        :param messages:
-        :param save_history:
-        :return:
+        Args:
+            inputs:
+            save_history:
+
+        Returns:
+
         """
 
-        messages = [dataclasses.asdict(m) for m in messages]
         response = openai.ChatCompletion.create(
             model=self.model,
-            messages=messages
+            **(inputs.to_json_inputs())
         )
         response = ChatGPTOutput.from_json(response)
+        message = response.choices[0].message
 
         if save_history:
             self.output_history.append(response)
+            self.message_history += inputs.messages
+            self.message_history.append(message)
 
         return response
 
-    def chat(self, messages: list[ChatGPTMessage], save_history: bool = True) -> str:
-        """Only the sentences returned when querying ChatGPT's API are retrieved.
-
-        :param messages:
-        :param save_history:
-        :return:
+    def chat(self,
+             inputs: ChatGPTInput,
+             save_history: bool = True) -> str:
         """
-        response = self.request(messages)
+
+        Args:
+            inputs:
+            save_history:
+
+        Returns:
+
+        """
+
+        response = self.request(
+            inputs=inputs, save_history=save_history
+        )
         choice = response.choices[0]
         message = choice.message
         content = message.content
 
-        if save_history:
-            self.message_history += messages
-            self.message_history.append(message)
         return content
+
+    def forget_history(self):
+        self.message_history = []
+        self.output_history = []
+
+
+if __name__ == '__main__':
+    pass
